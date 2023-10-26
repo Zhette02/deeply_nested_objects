b1c8032f0fdc9ec52a6b9a9d334dff04208868c4
zhette02 <zhette23@gmail.com>
2023-10-25 23:25:53 -0400

    comments on core beliefs in script.js

--- script.js
+++ script.js
@@ -4,23 +4,45 @@ let mind = {
 
     belief1: {
       name: "money is power",
-      
-
+      //positive 60% of the time NOT positive 40% of the time
+      conscious: true,
+      //verbal 40% NONverbal 60%
+      //catagory productive 50% destuctive 20% neutral 30%
+      //size is a random number between 1-10
+      //volume is a random number between 1-5
     },
 
     belief2: {
-      name: "i am in the wrong by default"
+      name: "i am in the wrong by default",
+      //positive 5% of the time NOT positive 95% of the time
+      conscious: false,
+      //verbal 3% NONverbal 97%
+      //catagory productive 3% destuctive 97% neutral 0%
+      //size is a random number between 1-10
+      //volume is a random number between 1-3
 
     },
 
     belief3: {
-      name: "the world is bad"
+      name: "the world is bad",
+      //positive 0% of the time NOT positive 100% of the time
+      conscious: true,
+      //verbal 50% NONverbal 50%
+      //catagory productive 30% destuctive 50% neutral 20%
+      //size is a random number between 1-10
+      //volume is a random number between 1-10
+      
 
     },
 
     belief4: {
-      name: "friends matter a lot"
-
+      name: "friends matter a lot",
+      //positive 80% of the time NOT positive 20% of the time
+      conscious: false,
+      //verbal 50% NONverbal 50%
+      //catagory productive 50% destuctive 50% neutral 0%
+      //size is a random number between 1-5
+      //volume is a random number between 1-10
     }
 
   },
@@ -30,6 +52,11 @@ let mind = {
     verbal: Boolean,
     positive: Boolean, 
     catagory: "string",
+    belief: "string",
+    size: Number,
+    volume: Number,
+    trigger: "string",
+
 
     // check: function() { 
     //   if (verbal == true){
